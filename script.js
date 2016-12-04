53eba4e7a7129b40c8ef3bd184fbbd2386e95342
nnisa <nisanoshin@gmail.com>
2016-12-04 15:24:10 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$('.message a').click(function () {
+	$('form').animate({
+		height : "toggle",
+		opacity : "toggle"
+	}, "slow");
+});
+
+$('.signup').click(function () {
+	console.log($(".name").val());
+	console.log($(".password").val());
+	console.log($(".email").val());
+	console.log($(".location").val());
+	
+	var data = { 
+		name : $(".name").val(),
+		password : $(".password").val(),
+		email : $(".email").val(),
+		location : $(".location").val()
+	};
+	
+	console.log(data);
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', "https://nfn80qiiqk.execute-api.us-east-1.amazonaws.com/prod", true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onload = function () {
+		if (xhr.status == 200) {
+		}
+	};
+	xhr.send(JSON.stringify(data));
+
+});
